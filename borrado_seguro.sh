c19a76387ea6e20acb3632c3091593990008e845
darthpedroo <pedrovillarino06@gmail.com>
2025-03-28 16:16:31 -0300

    added borrado seguro

---
+++ borrado_seguro.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+gzip $1 
+mv $1.gz ~/BASURA
+
---
+++ borrar_basura.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "aa"
+for file in ~/BASURA/*; do 
+    if [ -f "$file" ]; then 
+        file_timestamp=$(stat --format=%Z "$file") #Devuelve la fecha de modidicacion
+        current_timestamp=$(date +%s) 
+        time_difference=$((current_timestamp-file_timestamp))
+        seconds_to_delete=$((60 * 60 * 24 * 2))
+
+        if [ $time_difference -gt $seconds_to_delete ]
+        then
+            rm $file
+        else
+            echo "Pasaron: $time_difference segundos. Todavia no podes borrar el archivo"
+        fi
+    fi 
+done
